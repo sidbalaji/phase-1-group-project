8a9e23b2c7e71e052635cd5f881d827c200c2bf0
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-01-23 12:21:55 -0500

    css and html pages copied. html file has a selector added

---
+++ index.js
@@ -0,0 +1,12 @@
+// Write your code here...
+
+let firstMenuItem;
+
+cuisineSelector = document.querySelector("#cuisine")
+cuisineSelector.addEventListener('change',(e)=> {
+    console.log(e.target.value)
+    }
+                                 )
+
+
+fetch()
